1216328f4498917469f2e27a3fcb9aa0f474cd82
Inessa237 <43163950+Inessa237@users.noreply.github.com>
2018-09-11 16:18:25 +1000

    Update index.js

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -8,11 +8,11 @@ const cookbook = require('./alexa-cookbook.js');
 //TODO: The items below this comment need your attention.
 //=========================================================================================================================================
 
-const SKILL_NAME = 'Space Facts';
+const SKILL_NAME = 'Food Facts';
 const GET_FACT_MESSAGE = 'Here\'s your fact: ';
-const HELP_MESSAGE = 'You can say tell me a space fact, or, you can say exit... What can I help you with?';
+const HELP_MESSAGE = 'You can say tell me a food fact, or, you can say exit... What can I help you with?';
 const HELP_REPROMPT = 'What can I help you with?';
-const FALLBACK_MESSAGE = 'The Space Facts skill can\'t help you with that.  It can help you discover facts about space if you say tell me a space fact. What can I help you with?';
+const FALLBACK_MESSAGE = 'The Food Facts skill can\'t help you with that.  It can help you discover facts about food if you say tell me a food fact. What can I help you with?';
 const FALLBACK_REPROMPT = 'What can I help you with?';
 const STOP_MESSAGE = 'Goodbye!';
 
@@ -21,19 +21,19 @@ const STOP_MESSAGE = 'Goodbye!';
 //=========================================================================================================================================
 
 const data = [
-  'A year on Mercury is just 88 days long.',
-  'Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.',
-  'Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.',
-  'On Mars, the Sun appears about half the size as it does on Earth.',
-  'Earth is the only planet not named after a god.',
-  'Jupiter has the shortest day of all the planets.',
-  'The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.',
-  'The Sun contains 99.86% of the mass in the Solar System.',
-  'The Sun is an almost perfect sphere.',
-  'A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.',
-  'Saturn radiates two and a half times more energy into space than it receives from the sun.',
-  'The temperature inside the Sun can reach 15 million degrees Celsius.',
-  'The Moon is moving approximately 3.8 cm away from our planet every year.',
+  'McDonald’s fast food chains employ over 1.5 million people around the world.',
+  'Different parts of the world have their own local cuisine. The diets and general food habits of various cultures depend on social, religious, economic and safety factors as well as the availability of different foods.',
+  'Examples of food and cuisine that are popular or famous in certain areas of the world include hummus in the Middle East, apple pie in the USA, raw fish in Japan, cheese in France, roast meat and vegetables in England, curry in India and tortillas in Mexico.',
+  'There are around 2000 different plant types that humans use to cultivate food.',
+  'Examples of popular vegetables include lettuce, carrots, asparagus, cauliflower, broccoli, spinach, potatoes and onions.',
+  'The sweet potato is a root vegetable and is not closely related to the potato.',
+  'Cutting onions releases a gas which causes a stinging sensation when it comes into contact with your eyes. Your body produces tears to dilute the irritant and remove it from your eyes.',
+  'Pumpkins are usually labelled as vegetables but they contain seeds and are technically fruit.',
+  'China is the largest producer of garlic, producing over 10 million tons in 2008 and accounting for over 75% of world output.',
+  'Technically speaking, strawberries aren not even berries!',
+  'India is the world’s largest producer of bananas, producing nearly 22 million tons in 2007.',
+  'Around 70 million people suffer from food poisoning every year with around 7 million of these cases being fatal. Careful food storage, temperature control and preparation is necessary to avoid potentially dangerous bacteria, toxins and viruses.',
+  'Around 8% of children and 2% of adults have some kind of food allergy, this occurs when the body’s immune system incorrectly assumes a certain food protein is harmful and attacks it. Common examples of food allergies include reactions to peanuts, gluten and shellfish.',
 ];
 
 //=========================================================================================================================================
